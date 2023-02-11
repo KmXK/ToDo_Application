6b2c071b98450a43dd007b25c76f1ef83fde70fd
KmX <guydo.k@yandex.by>
2023-02-12 00:44:25 +0300

    Added first view

--- nest-app/src/main.ts
+++ nest-app/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.setBaseViewsDir(join(__dirname, 'views'));
+    app.set('view options', {layout: 'layouts/layout'});
+    app.setViewEngine('hbs');
+
+    await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then();
